72a236da57772a567510468b9759168666d87c23
Simon Lepel <simbo@simbo.de>
2015-11-18 19:51:56 +0100

    move pm2 apps config to project root

--- .provision/always.sh
+++ .provision/always.sh
@@ -8,7 +8,7 @@ echo_c "Install local npm packages..."
 npm i
 
 echo_c "Launch and dump app processes..."
-pm2 delete /vagrant/app/config/settings.json &> /dev/null
-pm2 start /vagrant/app/config/settings.json &> /dev/null
+pm2 delete /vagrant/pm2.json &> /dev/null
+pm2 start /vagrant/pm2.json &> /dev/null
 pm2 save
 pm2 status
