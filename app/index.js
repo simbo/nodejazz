7fac29a34120c9917858218114f2f69a6318b76a
Simon Lepel <simbo@simbo.de>
2015-11-18 20:22:57 +0100

    export error handlers into separate module

--- app/index.js
+++ app/index.js
@@ -29,20 +29,10 @@ config.routes.forEach(function(route) {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+app.use(errors.error404);
 
 // error handler
-app.use(function(err, req, res) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: err
-    });
-});
+app.use(errors.error);
 
 // listen
 server = app.listen(
