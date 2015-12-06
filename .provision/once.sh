8d67299aab0d6fbc3f201c9ca7bfb262dba6c9c1
Simon Lepel <simbo@simbo.de>
2015-12-06 12:55:30 +0100

    refactor provisioning; update vagrant box version and options

--- .provision/once.sh
+++ .provision/once.sh
@@ -4,43 +4,60 @@ source "/vagrant/.provision/provision-env.sh"
 
 # update sources, upgrade packages
 echo_c "Updating apt sources..."
-sudo apt-get -y -qq update
+
+apt-get -y -qq update
 
 # install apt packages
 echo_c "Installing apt packages..."
-sudo apt-get -y -qq install couchdb nginx
+
+apt-get -y -qq install couchdb nginx
 
 # configure nginx
 echo_c "Configuring nginx..."
-sudo apt-get -y -qq install nginx
-sudo cp -R $PROVISION_FILES/etc/nginx/* /etc/nginx
-sudo rm -f /etc/nginx/sites-enabled/default
-sudo ln -sf /etc/nginx/sites-available/vagrant /etc/nginx/sites-enabled/vagrant
-sudo service nginx restart
 
-# set NODE_PATH
-echo_c "Configuring NODE_PATH..."
+apt-get -y -qq install nginx
+
+cp -R $PROVISION_FILES/etc/nginx/* /etc/nginx
+rm -f /etc/nginx/sites-enabled/default
+ln -sf /etc/nginx/sites-available/vagrant /etc/nginx/sites-enabled/vagrant
+
+service nginx restart
+
+# set env
+echo_c "Configuring environment..."
+
 export NODE_PATH="/vagrant/app"
-if [ -z $(grep -q "export NODE_PATH=${NODE_PATH}" "${HOME}/.zshenv") ]; then
-  cat >>${HOME}/.zshenv <<EOL
 
-# node path for requiring modules
-export NODE_PATH=${NODE_PATH}
+NVM_PATH_SCRIPT=$(cat <<EOL
+
+# node require path
+export NODE_PATH="${NODE_PATH}"
 EOL
+)
+
+if ! grep -q "# node require path" /etc/profile; then
+  echo "$NVM_PATH_SCRIPT" >>/etc/profile
+fi
+
+if ! grep -q "# node require path" /home/vagrant/.zshenv; then
+  echo "$NVM_PATH_SCRIPT" >>/home/vagrant/.zshenv
 fi
 
 # install global npm packages
 echo_c "Installing global npm packages..."
-npm i -g pm2@0.15.10 gulp@3.9.0 npm-check-updates
+
+su vagrant -c "npm i -g pm2@0.15.10 gulp@3.9.0 npm-check-updates"
 
 # setup pm2 autostart
 echo_c "Configuring pm2..."
-sudo su -c "env PATH=$PATH:/home/vagrant/.nvm/versions/node/v5.0.0/bin pm2 startup ubuntu -u vagrant --hp /home/vagrant"
+
+env PATH=/home/vagrant/.nvm/versions/node/current/bin:$PATH pm2 startup ubuntu -u vagrant --hp /home/vagrant
 
 # customize .zshrc
 echo_c "Customizing .zshrc..."
-if [ -z $(grep -q "cd /vagrant" "${HOME}/.zshrc") ]; then
-  cat >>${HOME}/.zshenv <<EOL
+
+if ! grep -q "cd /vagrant" /home/vagrant/.zshrc; then
+  cat >>/home/vagrant/.zshrc <<EOL
 
 cd /vagrant
 EOL
